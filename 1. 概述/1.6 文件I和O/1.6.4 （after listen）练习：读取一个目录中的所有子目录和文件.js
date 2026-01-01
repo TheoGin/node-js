5272270c39f889ee80c8c04886f562d6cbaa1bad
傅朝俊 <fuchaojun@szkingdom.com>
2026-01-01 13:28:24 +0800

    fix：1.6 文件I/O

--- 1. 概述/1.6 文件I和O/1.6.4 （after listen）练习：读取一个目录中的所有子目录和文件.js
+++ 1. 概述/1.6 文件I和O/1.6.4 （after listen）练习：读取一个目录中的所有子目录和文件.js
@@ -2,68 +2,90 @@ const fs = require("node:fs");
 const path = require("node:path");
 
 class File {
-  constructor(fileName) {
-    this.name = fileName;
-    // this.name = this.getAbsolutePath(fileName);
-    // console.log(this.name);
-    const absolutePath = this.getAbsolutePath(fileName);
-    this.ext = path.extname(absolutePath);
-
-    const stats = fs.statSync(absolutePath);
-    this.isFile = stats.isFile();
-    this.size = stats.size;
-    this.createTime = new Date(stats.birthtime);
-    this.updateTime = new Date(stats.mtime);
+  constructor(fileName, name, ext, isFile, size, createTime, updateTime) {
+    this.fileName = fileName;
+    this.name = name;
+    this.ext = ext;
+    this.isFile = isFile;
+    this.size = size;
+    this.createTime = createTime;
+    this.updateTime = updateTime;
   }
 
-  /*async init(fileName) {
-    const stats = await fs.promises.stat(this.getAbsolutePath(this.name));
-    console.log(stats);
-  }*/
-
-  getAbsolutePath(fileName) {
-    return path.resolve(__dirname, fileName);
+  static async getFile(fileName) {
+    const name = path.basename(fileName);
+    const ext = path.extname(fileName);
+    const stats = await fs.promises.stat(fileName);
+    const isFile = stats.isFile();
+    const size = stats.size;
+    const createTime = new Date(stats.birthtime);
+    const updateTime = new Date(stats.mtime);
+    return new File(fileName, name, ext, isFile, size, createTime, updateTime);
   }
 
   async getChildren() {
-    if (!this.isFile) {
-      const fileNames = await fs.promises.readdir(this.getAbsolutePath(this.name));
-      const files = [];
-      for (const fileName of fileNames) {
-        // console.log(this.getAbsolutePath(`./${this.name}/${fileName}`));
-        files.push(new File(`${this.name}/${fileName}`));
-      }
-      return files;
+    if (this.isFile) {
+      // 文件不可能有子文件
+      return [];
     }
-    return [];
+    const fileNames = await fs.promises.readdir(this.fileName);
+    const files = fileNames.map(fileName => {
+      const filePath = path.resolve(this.fileName, fileName);
+      return  File.getFile(filePath)
+    });
+    // return Promise.allSettled(files);
+    return Promise.all(files);
   }
 
   async getContent(isBuffer = false) {
-    if (!this.isFile) {
-      return null;
+    if (this.isFile) {
+      if (isBuffer) {
+        return await fs.promises.readFile(this.fileName);
+      }
+      return await fs.promises.readFile(this.fileName, "utf-8");
     }
-    return await fs.promises.readFile(path.resolve(__dirname, this.name), isBuffer ? "" : "utf-8");
+    return null;
   }
+
+}
+
+async function readDir(dirName) {
+  // console.log('dirName', dirName);
+  const file = await File.getFile(dirName);
+  return await file.getChildren();
 }
 
 async function test() {
-  const file = new File("myFiles");
-  const files = await file.getChildren();
+  // const file = await readDir(path.resolve(__dirname, "myFiles/a.txt"));
+  // console.log(await file.getContent(true));
 
-  printContent(files);
+  const dirName = path.resolve(__dirname, "myFiles");
+  const files = await readDir(dirName);
+  // console.log(files);
+  recurve(files);
 
-  async function printContent(files) {
+  async function recurve(files) {
     for (const f of files) {
-      if (!f.isFile) {
-        const children = await f.getChildren();
-        await printContent(children);
-        continue;
+      if (f.isFile) {
+        console.log(await f.getContent(f.ext === ".jpeg"));
+      } else {
+        const children =await readDir(f.fileName);
+        // console.log(children);
+        recurve(children);
       }
-      const content = await f.getContent(f.ext === '.jpeg');
-      // console.log('filename: ', f.name);
-      console.log(content);
     }
   }
 }
 
 test();
+
+/*
+<Buffer ff d8 ff e0 00 10 4a 46 49 46 00 01 01 00 00 01 00 01 00 00 ff db 00 43 00 08 06 06 07 06 05 08 07 07 07 09 09 08 0a 0c 14 0d 0c 0b 0b 0c 19 12 13 0f ... 13255 more bytes>
+3.txt
+<Buffer ff d8 ff e0 00 10 4a 46 49 46 00 01 01 00 00 01 00 01 00 00 ff db 00 43 00 08 06 06 07 06 05 08 07 07 07 09 09 08 0a 0c 14 0d 0c 0b 0b 0c 19 12 13 0f ... 13255 more bytes>
+6.txt
+a.txt: IO的速度往往低于内存和CPU的交互速度
+3.txt
+abc写入abc写入abc写入abc写入abc写入abc写入
+t.txt
+ */
